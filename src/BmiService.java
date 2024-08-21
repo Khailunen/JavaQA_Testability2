aeb37c9f138a252dbbf10a8a02a222ceea0b3311
Karina Khaltunen <khaltunen@bk.ru>
2024-08-21 22:16:44 +0300

    modified code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate (double bodyWeightKg, double heightMetre) {
+    public int calculate(double bodyWeightKg, double heightMetre) {
         double bmi = bodyWeightKg / heightMetre / heightMetre;
         return (int) bmi;
     }
